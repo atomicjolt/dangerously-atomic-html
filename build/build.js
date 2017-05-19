0902d4ac6f24c88ea2f588510b5e1994c0fcdb16
dittonjs <jditton.atomic@gmail.com>
2017-05-18 19:53:37 -0600

    release

--- build/build.js
+++ build/build.js
@@ -17,29 +17,35 @@ var _lodash2 = _interopRequireDefault(_lodash);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function dynamicReact(html) {
-  var predicate = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {
+  var visitorFunc = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {
     return false;
   };
   var key = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'dynamic_component';
 
   var root = document.createElement('div');
   root.innerHTML = html;
-  var combinedPredicate = function combinedPredicate(node) {
-    var result = predicate(node);
-    if (result || result === null) {
+  var combinedVisitor = function combinedVisitor(node) {
+    var result = visitorFunc && visitorFunc(node);
+    if (visitorFunc !== null && (result || result === null)) {
       // return null to not render something for the node
       return result;
     }
-    return defaultPredicate(node);
+    return defaultVisitorFunc(node);
   };
-  return recursiveDynamicReact(root, combinedPredicate, key);
+  if (root.childNodes.length === 1) {
+    return recursiveDynamicReact(root.childNodes[0], combinedVisitor, key);
+  } else {
+    return _lodash2.default.map(root.childNodes, function (childNode, index) {
+      return recursiveDynamicReact(childNode, combinedVisitor, key, 0, index);
+    });
+  }
 }
 
-function recursiveDynamicReact(node, predicate, key) {
+function recursiveDynamicReact(node, visitorFunc, key) {
   var level = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
   var index = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 0;
 
-  var result = predicate(node);
+  var result = visitorFunc(node);
   if (!result) {
     return null;
   }
@@ -58,13 +64,13 @@ function recursiveDynamicReact(node, predicate, key) {
     Component,
     Object.assign({ key: key + '_' + level + '_' + index }, result.props),
     _lodash2.default.map(node.childNodes, function (childNode, i) {
-      return recursiveDynamicReact(childNode, predicate, level + 1, i);
+      return recursiveDynamicReact(childNode, visitorFunc, level + 1, i);
     })
   );
 }
 
 var voidTags = ['IMG', 'INPUT'];
-function defaultPredicate(node) {
+function defaultVisitorFunc(node) {
   if (node.nodeName === '#text') {
     return {
       type: 'text',
